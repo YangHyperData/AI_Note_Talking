96e84426c191dc4d6dab7a5502adc6dfe32b2196
YangStone <ntg0311@gmail.com>
2024-11-09 21:41:57 +0700

    Authentication

--- app/page.js
+++ app/page.js
@@ -1,11 +1,39 @@
+"use client"
 import { Button } from "@/components/ui/button";
+import { api } from "@/convex/_generated/api";
+import { UserButton, useUser } from "@clerk/nextjs";
+import { useMutation } from "convex/react";
 import Image from "next/image";
+import { useEffect } from "react";
 
 export default function Home() {
+
+  const { user } = useUser();
+  const createUser = useMutation(api.user.createUser);
+
+  useEffect(() => {
+    user&&CheckUser();
+  },[user])
+
+  const CheckUser = async () => {
+    const result = await createUser({
+      email: user?.primaryEmailAddress?.emailAddress,
+      imageUrl: user?.imageUrl,
+      userName: user?.fullName
+    });
+    console.log(result);
+  }
   return (
     <div>
       <h2>YangStone</h2>
       <Button>Hehe</Button>
+      <UserButton
+        appearance={{
+          elements: {
+            avatarBox: 'h-16 w-16', // Adjust the size here
+          },
+        }}
+      />
     </div>
   );
 }
--- app/provider.js
+++ app/provider.js
@@ -2,7 +2,7 @@
 import React from 'react'
 import { ConvexProvider, ConvexReactClient } from "convex/react";
 
-function Provider(children) {
+function Provider({children}) {
     const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL);
     return (
         <div>
