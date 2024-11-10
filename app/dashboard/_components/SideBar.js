39bebb5be3f98f25b2c36c9e4559415dbf0ce756
YangStone <ntg0311@gmail.com>
2024-11-10 22:10:32 +0700

    Upload PDF File (Dialog Form)

--- app/dashboard/_components/SideBar.js
+++ app/dashboard/_components/SideBar.js
@@ -3,13 +3,16 @@ import { Progress } from '@/components/ui/progress'
 import { Layout, Shield } from 'lucide-react'
 import Image from 'next/image'
 import React from 'react'
+import UploadPdfDialog from './UploadPdfDialog'
 
 function SideBar() {
     return (
         <div className='shadow-2xl h-screen p-7'>
             <Image src={'/logo.svg'} width={50} height={50} alt='logo' />
             <div className='mt-10'>
-                <Button className="w-full">+ Upload PDF</Button>
+                <UploadPdfDialog>
+                    <Button className="w-full">+ Upload PDF</Button>
+                </UploadPdfDialog>
                 <div className='flex gap-2 items-center p-5 mt-5 hover:bg-slate-100 rounded-lg cursor-pointer'>
                     <Layout />
                     <h2>Workspace</h2>
---
+++ app/dashboard/_components/UploadPdfDialog.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import {
+    Dialog,
+    DialogClose,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog"
+import { Input } from '@/components/ui/input'
+import { Button } from '@/components/ui/button'
+
+function UploadPdfDialog({ children }) {
+    return (
+        <Dialog>
+            <DialogTrigger asChild>
+                {children}
+            </DialogTrigger>
+            <DialogContent>
+                <DialogHeader>
+                    <DialogTitle>Upload Pdf File</DialogTitle>
+                    <DialogDescription asChild>
+                        <div className=''>
+                            <h2 className='mt-5'>Select a file to Upload</h2>
+                            <div className='gap-2 p-3 rounded-md border'>
+
+                                <input type='file' accept='application/pdf' />
+                            </div>
+                            <div className='mt-2'>
+                                <label>File Name *</label>
+                                <Input placeholder="File Name" />
+                            </div>
+                        </div>
+                    </DialogDescription>
+                </DialogHeader>
+                <DialogFooter className="sm:justify-end">
+                    <DialogClose asChild>
+                        <Button type="button" variant="secondary">
+                            Close
+                        </Button>
+                    </DialogClose>
+                    <Button>Upload</Button>
+                </DialogFooter>
+            </DialogContent>
+        </Dialog>
+    )
+}
+
+export default UploadPdfDialog
